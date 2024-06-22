15999050c7c10b089e72eb989fcfbc86c1392b98
João Thallis <joaothallis@icloud.com>
2024-06-22 20:14:34 -0300

    Run goimports on save

--- init.lua
+++ init.lua
@@ -83,7 +83,27 @@ local lsp = require("lsp-zero")
 
 lsp.preset("recommended")
 
-lsp.format_on_save({servers = {["gopls"] = {'go'}}})
+-- Run gofmt/gofmpt, import packages automatically on save
+vim.api.nvim_create_autocmd('BufWritePre', {
+  group = vim.api.nvim_create_augroup('setGoFormatting', { clear = true }),
+  pattern = '*.go',
+  callback = function()
+    local params = vim.lsp.util.make_range_params()
+    params.context = { only = { "source.organizeImports" } }
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 2000)
+    for _, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          vim.lsp.util.apply_workspace_edit(r.edit, "utf-16")
+        else
+          vim.lsp.buf.execute_command(r.command)
+        end
+      end
+    end
+
+    vim.lsp.buf.format()
+  end
+})
 
 lsp.on_attach(function(client, bufnr) lsp.default_keymaps({buffer = bufnr}) end)
 
